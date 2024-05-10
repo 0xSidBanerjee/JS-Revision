ebeaacbafe4963356cd33e0529508e512b0ff35d
Siddhant <siddhantban02@gmail.com>
2024-05-10 09:18:42 +0530

    Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict" //Treat all the JS code below as a newer version of JS
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object
