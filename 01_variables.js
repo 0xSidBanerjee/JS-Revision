c8df7eed6f85b0f02435b5c6249d5726eb67f725
Siddhant <siddhantban02@gmail.com>
2024-05-10 09:02:59 +0530

    01 Variables

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 69420;
+let accountEmail= 'batman@batcave.com';
+var accountPassword = 'ironmanSUCKS';
+
+//Changing of accountId variable isn't allowed anymore
+
+//accountEmail and accountPassword can be changed again 
+
+/*
+Prefer not to use var for its issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword]);
