f344912719906456ed1689b422357121677d7d12
Siddhant <siddhantban02@gmail.com>
2024-05-10 20:03:23 +0530

    Added Strings

---
+++ 01_Basics/04_Strings.js
@@ -0,0 +1,8 @@
+const name = "Paul";
+const house = "Atreides"
+
+// console.log(name+house); //Outdated method, recommended not to use
+
+console.log(`${name} ${house}`);
+
+const gameName = new String("High on Life");
