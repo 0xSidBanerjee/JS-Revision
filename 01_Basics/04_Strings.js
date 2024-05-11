d5bade7a0c0f2a345850b46202b766a2af622a60
Siddhant <siddhantban02@gmail.com>
2024-05-11 11:27:50 +0530

    Completed String functions

--- 01_Basics/04_Strings.js
+++ 01_Basics/04_Strings.js
@@ -6,3 +6,4 @@ const house = "Atreides"
 console.log(`${name} ${house}`);
 
 const gameName = new String("High on Life");
+console.table([gameName[0], gameName.length, gameName.toUpperCase(), gameName.indexOf('m'), gameName.charAt(3), gameName.substring(0,4), gameName.slice(-4), gameName.replace('Life', 'Weed'), gameName.includes('High'), gameName.split(' ')]);
