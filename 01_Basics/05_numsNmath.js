977786d34e4753e4ec65ee54a7437ca060ce61f0
Siddhant <siddhantban02@gmail.com>
2024-05-11 11:39:17 +0530

    Completed Numbers and Maths

---
+++ 01_Basics/05_numsNmath.js
@@ -0,0 +1,16 @@
+const score = new Number(33);
+
+console.log(score.toString());
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// To generate a random number between 33 and 42
+console.log(Math.floor(Math.random() * (42 - 33 + 1) + 33));
