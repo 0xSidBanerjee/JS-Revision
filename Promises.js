fbb1c2ba9e72712725b597002e807744346af53b
Siddhant <siddhantban02@gmail.com>
2024-05-18 10:24:06 +0530

    Added async await syntax

--- Promises.js
+++ Promises.js
@@ -1,15 +1,40 @@
 const fs = require("fs");
-function readFile(){
-    return new Promise((resolve)=>{
-        fs.readFile('a.txt', 'utf-8', (err,data)=>{
-            resolve(data);
+const { resolve } = require("path");
+// function readFile(){
+//     return new Promise((resolve)=>{
+//         fs.readFile('a.txt', 'utf-8', (err,data)=>{
+//             resolve(data);
+//         })
+//     })
+// }
+
+// function newFile(data){
+//     console.log(data);
+// }
+
+// readFile().then(newFile);
+// This is supposed to be a syntactical sugar for callbacks but also using promises instead we avoid something known as the callback hell.
+
+function tangerine(filePath){
+    return new Promise((resolve, reject)=>{
+        fs.readFile(filePath, 'utf-8', (err, data)=>{
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(data);
+            }
         })
     })
 }
 
-function newFile(data){
-    console.log(data);
+async function lemon(){
+    try{
+        const value = await tangerine('a.txt');
+        console.log(value);
+    }
+    catch(err){
+        console.log(err);
+    }
+    
 }
-
-readFile().then(newFile);
-// This is supposed to be a syntactical sugar for callbacks but also using promises instead we avoid something known as the callback hell.
