1fa7eca9af7fee00b35dd04098dd4dd84bb30801
Siddhant <siddhantban02@gmail.com>
2024-05-30 23:17:43 +0530

    OOP concepts

---
+++ 08_OOP/01_ObjectOriented.js
@@ -0,0 +1,9 @@
+// JS actually doesn't have classes, though classes was introducted in ES6
+// But actually JS is a prototype based language
+// And classes are simply syntactic sugar over existing prototype-type based inheritance mechanisms
+
+// What is am object
+// Collection of properties and methods
+
+// 4 pillars
+// Abstraction Encapsulation Inheritance Polymorphism
